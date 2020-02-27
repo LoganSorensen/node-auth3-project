2bde6dcc53bf2d646a77a1819773171e2e15c70b
Logan Sorensen <56973914+LoganSorensen@users.noreply.github.com>
2020-02-26 18:11:41 -0800

    Revert "Logan sorensen"

--- index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-const server =require('./api/server.js');
-
-const port = process.env.PORT || 5002;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
